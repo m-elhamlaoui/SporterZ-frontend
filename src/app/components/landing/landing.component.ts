e7268b91ace7b587d6c2ef207aee8bccf154d6f8
Mohammed-Yassine BOUMEHDI <myboumehdi.2002@gmail.com>
2024-03-27 05:36:45 +0000

    Add Dark/Ligh modes switcher

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [
+    FooterComponent,
+    NgIf
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
+  isDark : Boolean = false;
 
+  switchMode() {
+    this.isDark = !this.isDark;
+  }
 }
