d0a6d6cc85e4884759625c504e2da2b16d25582b
Said NAJIM <saidnajim.ma@gmail.com>
2024-04-23 19:11:30 +0100

    Integrate backend with frontend

---
+++ src/app/components/matches/matches.interface.ts
@@ -0,0 +1,17 @@
+interface Match {
+  idMatch: number;
+  nameStadium: string;
+  cityStadium: string;
+  datetimeMatch: string;
+  statusMatch: string;
+  idLeague: number;
+  nameLeague: string;
+  idHomeTeam: number;
+  nameHomeTeam: string;
+  goalsHomeTeam: number;
+  idAwayTeam: number;
+  nameAwayTeam: string;
+  goalsAwayTeam: number;
+}
+
+export { Match };
