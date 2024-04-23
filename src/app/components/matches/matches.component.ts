42f9096641764039257027775f20712905a69ef5
Said NAJIM <saidnajim.ma@gmail.com>
2024-04-23 19:11:30 +0100

    Integrate backend with frontend

--- src/app/components/matches/matches.component.ts
+++ src/app/components/matches/matches.component.ts
@@ -1,67 +1,37 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
-
-interface Match {
-  date: string;
-  match_time: string;
-  room_link: string;
-  league: string;
-  league_logo: string;
-  status: string;
-
-  club1: string;
-  logo1: string;
-  score1: string;
-  votes1: string;
-
-  club2: string;
-  logo2: string;
-  score2: string;
-  votes2: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { Match } from './matches.interface';
+import { MatchesService } from './matches.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-matches',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './matches.component.html',
-  styleUrl: './matches.component.scss'
+  styleUrls: ['./matches.component.scss'],
+  providers: [MatchesService], 
 })
-export class MatchesComponent {
-  matches: Match[] = [{
-    "date": "12 Aug",
-    "match_time": "19:00",
-    "room_link": "https://www.google.com",
-    "league": "Champions League",
-    "league_logo": "https://upload.wikimedia.org/wikipedia/fr/b/bf/UEFA_Champions_League_logo_2.svg",
-    "status": "Live",
-    "club1": "Real Madrid",
-    "logo1": "https://upload.wikimedia.org/wikipedia/fr/c/c7/Logo_Real_Madrid.svg",
-    "score1": "7",
-    "votes1": "1.48",
-    "club2": "Arsenal FC",
-    "logo2": "https://upload.wikimedia.org/wikipedia/fr/5/53/Arsenal_FC.svg",
-    "score2": "0",
-    "votes2": "4.98"
-  }, {
-    "date": "12 Aug",
-    "match_time": "19:00",
-    "room_link": "https://www.google.com",
-    "league": "Champions League",
-    "league_logo": "https://upload.wikimedia.org/wikipedia/fr/b/bf/UEFA_Champions_League_logo_2.svg",
-    "status": "Live",
-    "club1": "Real Madrid",
-    "logo1": "https://upload.wikimedia.org/wikipedia/fr/c/c7/Logo_Real_Madrid.svg",
-    "score1": "7",
-    "votes1": "1.48",
-    "club2": "Arsenal FC",
-    "logo2": "https://upload.wikimedia.org/wikipedia/fr/5/53/Arsenal_FC.svg",
-    "score2": "0",
-    "votes2": "4.98"
-  }
-  ]
+export class MatchesComponent implements OnInit {
+  matches: Match[] = [];
+  loading: boolean = true; 
 
-  redirectToRoom(roomLink: string) {
-    window.location.href = roomLink;
+  constructor(private matchesService: MatchesService) {}
+
+  ngOnInit(): void {
+    this.fetchMatches();
+  }
+
+  fetchMatches(): void {
+    this.matchesService.getMatches().subscribe(
+      (matches) => {
+        this.matches = matches;
+        this.loading = false; // Set loading to false when matches are fetched
+      },
+      (error) => {
+        console.error('Error fetching matches:', error);
+        this.loading = false; 
+      }
+    );
   }
 }
