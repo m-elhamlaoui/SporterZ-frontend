c36c680c8c87df112d0a4a0088ff8771b90ad71e
ilyasabdellaoui <ilyas.abdellaoui21@gmail.com>
2024-05-02 21:33:51 +0100

    [FEAT] Profile: implement profile section

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,174 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MaterialModule } from '../../material-modules';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProfileService } from '../../services/profile.service';
+import { TokenStorageService } from '../../services/token-storage.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [         
+    ReactiveFormsModule
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
 
+export class ProfileComponent implements OnInit {
+  pictureForm: FormGroup = new FormGroup({});
+  profileForm: FormGroup = new FormGroup({});
+  passwordForm: FormGroup = new FormGroup({});
+
+  profileData: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private profileService: ProfileService,
+    private tokenStorageService: TokenStorageService,
+  ) {}
+
+  ngOnInit() {
+    this.pictureForm = this.fb.group({
+      picture: ['', Validators.required]
+    });
+
+    this.profileForm = this.fb.group({
+      firstname: [''],
+      lastname: [''],
+      username: [''],
+      email: ['', [Validators.email]],
+    });
+
+    this.passwordForm = this.fb.group({
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validators: this.passwordMatchValidator });
+    this.getProfile();
+  }
+
+  passwordMatchValidator(form: FormGroup): any{
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    if (password === null || confirmPassword === null) {
+      return { 'mismatch': true };
+    }        
+    return password === confirmPassword ? null : { 'mismatch': true };
+  }
+
+  getProfile() {
+    return this.profileService.getProfile().subscribe({
+      next: (data: any) => {
+        this.profileData = data;
+        this.profileForm.patchValue(data);
+      },
+      error: (e) => {
+        if (e.status === 403) {
+          this.tokenStorageService.logout();
+        }
+      },
+    });
+  }
+
+  onPictureSubmit() {
+    if (this.pictureForm.valid) {
+      const formData = new FormData();
+      formData.append('picture', this.pictureForm.get('picture')?.value.files[0]);
+
+      this.profileService.updateProfilePicture(formData).subscribe({
+        next: (response: any) => {
+          console.log('Picture uploaded successfully', response);
+          alert("Picture uploaded successfully.");
+        },
+        error: (error: any) => {
+          console.error('Error uploading picture', error);
+          alert("Failed to upload picture.");
+        }
+      });
+    } else {
+      alert("Please select a picture to upload.");
+    }
+  }
+
+  deletePicture() {
+    this.profileService.deleteProfilePicture().subscribe({
+      next: (response: any) => {
+        console.log('Picture deleted successfully', response);
+        alert("Picture deleted successfully.");
+        this.profileData.picture = null;
+      },
+      error: (error: any) => {
+        console.error('Error deleting picture', error);
+        alert("Failed to delete picture.");
+      }
+    });
+  }
+
+  onProfileSubmit() {
+    if (this.profileForm.valid) {
+      if (!this.profileData) {
+        console.error('Profile data is not loaded yet.');
+        alert('Profile data is not available. Please try again later.');
+        return;
+      }
+  
+      const formData = this.profileForm.value;
+      const updatedData: { [key: string]: any } = {};
+  
+      Object.keys(this.profileData).forEach(key => {
+        if (formData[key] !== undefined && formData[key] !== this.profileData[key]) {
+          updatedData[key] = formData[key];
+        }
+      });
+
+      if (Object.keys(updatedData).length > 0) {
+        this.profileService.updateProfile(updatedData).subscribe({
+          next: (data) => {
+            console.log('Profile data updated successfully');
+            alert("Profile data updated successfully");
+            this.getProfile();
+          },
+          error: (e) => {
+            if (e.status === 403) {
+              alert("Error updating profile data");
+              this.tokenStorageService.logout();
+            }
+          },
+        });
+      } else {
+        console.log("No changes to update.");
+        alert("No changes detected.");
+      }
+    }
+  }  
+
+  onPasswordSubmit() {
+    if (this.passwordForm.valid) {
+      const passwordData = {
+        password: this.passwordForm.get('password')?.value
+      };
+  
+      this.profileService.updatePassword(passwordData).subscribe({
+        next: (data) => {
+          console.log('Password updated successfully');
+          alert("Password updated successfully");
+          this.passwordForm.reset();
+        },
+        error: (e) => {
+          console.error('Error updating password', e);
+          if (e.status === 403) {
+            alert("Unauthorized: Error updating password");
+            this.tokenStorageService.logout();
+          } else {
+            alert("Error occurred while updating password.");
+          }
+        },
+      });
+    } else {
+      if (this.passwordForm.errors?.['mismatch']) {
+        alert('Passwords do not match!');
+      } else {
+        alert('Please check your password form for errors.');
+      }
+    }
+  }
 }
---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { TokenStorageService } from './token-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  private baseUrl = 'http://localhost:8085/api/profile';
+  private httpOptions: any;
+
+  constructor(
+    private http: HttpClient,
+    private tokenStorageService: TokenStorageService
+  ) {
+      this.httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + this.tokenStorageService.getToken(),
+      }),
+    };
+  }
+
+  getProfile() {
+    return this.http.get<any>(`${this.baseUrl}/me`, this.httpOptions);
+  }
+
+  updateProfilePicture(picture: any) {
+    return this.http.post(`${this.baseUrl}/update-picture`, picture, this.httpOptions);
+  }
+
+  deleteProfilePicture() {
+    return this.http.delete(`${this.baseUrl}/delete-picture`, this.httpOptions);
+  }
+
+  updateProfile(profileData: any) {
+    return this.http.post(`${this.baseUrl}/update`, profileData, this.httpOptions);
+  }
+
+  updatePassword(password: any) {
+    return this.http.post(`${this.baseUrl}/update-password`, password, this.httpOptions);
+  }
+
+  deleteProfile() {
+    return this.http.delete(`${this.baseUrl}/delete`, this.httpOptions);
+  }
+}
